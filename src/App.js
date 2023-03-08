d5148ec38498b6f4a798321dd0c94b2006d97849
Dobby <apple@Dobbyde-MacBook-Pro.local>
2023-03-08 16:59:18 -0500

    add about me section and it's styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "./components/navbar";
+import AboutMe from "./components/about-me";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <AboutMe />
     </div>
   );
 }
